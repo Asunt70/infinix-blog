b8541a81702f80c2005048c2ab8d3fdf2ea4d5f5
Asunt70 <sebaj348@gmail.com>
2024-09-20 15:26:06 -0400

    fixed error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { Suspense } from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Navbar from "../components/NavBar";
 import Card from "../components/card";
@@ -16,7 +15,6 @@ interface BlogPost {
 }
 
 export default function SearchResults() {
-  // Logic
   const searchParams = useSearchParams();
   const query = searchParams.get("q") || "";
   const [filteredPosts, setFilteredPosts] = useState<BlogPost[]>([]);
@@ -42,53 +40,50 @@ export default function SearchResults() {
     }
   }, [query]);
 
-  //Contents
-
   return (
     <>
       <Navbar />
-
-      <div className={styles.pageContainer}>
-        <h1 className={styles.heading}>
-          Search Results for{" "}
-          <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className={styles.pageContainer}>
+          <h1 className={styles.heading}>
+            Search Results for{" "}
             <span className={styles.searchQuery}>&quot;{query}&quot;</span>:
-          </Suspense>
-        </h1>
-        <ul className={styles.searchList}>
-          {filteredPosts.length > 0 ? (
-            filteredPosts.map((post) => (
-              <li key={post.id} className={styles.searchResult}>
-                <Card
-                  title={post.title}
-                  description={post.description}
-                  imageSrc={post.image}
-                  imageAlt={post.title}
-                  url={post.url}
-                />
-              </li>
-            ))
-          ) : (
-            <li>No results found.</li>
-          )}
-        </ul>
+          </h1>
+          <ul className={styles.searchList}>
+            {filteredPosts.length > 0 ? (
+              filteredPosts.map((post) => (
+                <li key={post.id} className={styles.searchResult}>
+                  <Card
+                    title={post.title}
+                    description={post.description}
+                    imageSrc={post.image}
+                    imageAlt={post.title}
+                    url={post.url}
+                  />
+                </li>
+              ))
+            ) : (
+              <li>No results found.</li>
+            )}
+          </ul>
 
-        <Link href="/" className={styles.backToHome}>
-          Back To Home{" "}
-          <span className={styles.arrowContainer}>
-            <svg
-              className="arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#e8eaed"
-            >
-              <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-            </svg>
-          </span>
-        </Link>
-      </div>
+          <Link href="/" className={styles.backToHome}>
+            Back To Home{" "}
+            <span className={styles.arrowContainer}>
+              <svg
+                className="arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e8eaed"
+              >
+                <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+              </svg>
+            </span>
+          </Link>
+        </div>
+      </Suspense>
     </>
   );
 }
