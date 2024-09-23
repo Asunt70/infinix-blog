fb141848905389e02bef9d66baa7ac25b4c8d351
Asunt70 <sebaj348@gmail.com>
2024-09-23 12:54:14 -0400

    v.0.1.0

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-
+import Navbar from "../components/NavBar";
 import Card from "../components/card";
 import Link from "next/link";
 import styles from "./page.module.css";
@@ -14,7 +14,20 @@ interface BlogPost {
   url: string;
 }
 
-export default function SearchResults() {
+const SearchResultsWrapper = () => {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.pageContainer}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchResults />
+        </Suspense>
+      </div>
+    </>
+  );
+};
+
+const SearchResults = () => {
   const searchParams = useSearchParams();
   const query = searchParams?.get("q") || "";
   const [filteredPosts, setFilteredPosts] = useState<BlogPost[]>([]);
@@ -25,12 +38,10 @@ export default function SearchResults() {
       const posts: BlogPost[] = await response.json();
       const results = posts.filter(
         (post) =>
-          post.title.toLowerCase().includes((query as string).toLowerCase()) ||
-          post.description
-            .toLowerCase()
-            .includes((query as string).toLowerCase()) ||
-          post.url.toLowerCase().includes((query as string).toLowerCase()) ||
-          post.image.toLowerCase().includes((query as string).toLowerCase()),
+          post.title.toLowerCase().includes(query.toLowerCase()) ||
+          post.description.toLowerCase().includes(query.toLowerCase()) ||
+          post.url.toLowerCase().includes(query.toLowerCase()) ||
+          post.image.toLowerCase().includes(query.toLowerCase()),
       );
       setFilteredPosts(results);
     };
@@ -42,47 +53,45 @@ export default function SearchResults() {
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className={styles.pageContainer}>
-          <h1 className={styles.heading}>
-            Search Results for{" "}
-            <span className={styles.searchQuery}>&quot;{query}&quot;</span>:
-          </h1>
-          <ul className={styles.searchList}>
-            {filteredPosts.length > 0 ? (
-              filteredPosts.map((post) => (
-                <li key={post.id} className={styles.searchResult}>
-                  <Card
-                    title={post.title}
-                    description={post.description}
-                    imageSrc={post.image}
-                    imageAlt={post.title}
-                    url={post.url}
-                  />
-                </li>
-              ))
-            ) : (
-              <li>No results found.</li>
-            )}
-          </ul>
+      <h1 className={styles.heading}>
+        Search Results for{" "}
+        <span className={styles.searchQuery}>&quot;{query}&quot;</span>:
+      </h1>
+      <ul className={styles.searchList}>
+        {filteredPosts.length > 0 ? (
+          filteredPosts.map((post) => (
+            <li key={post.id} className={styles.searchResult}>
+              <Card
+                title={post.title}
+                description={post.description}
+                imageSrc={post.image}
+                imageAlt={post.title}
+                url={post.url}
+              />
+            </li>
+          ))
+        ) : (
+          <li>No results found.</li>
+        )}
+      </ul>
 
-          <Link href="/" className={styles.backToHome}>
-            Back To Home{" "}
-            <span className={styles.arrowContainer}>
-              <svg
-                className="arrow"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#e8eaed"
-              >
-                <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-              </svg>
-            </span>
-          </Link>
-        </div>
-      </Suspense>
+      <Link href="/" className={styles.backToHome}>
+        Back To Home{" "}
+        <span className={styles.arrowContainer}>
+          <svg
+            className="arrow"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+          >
+            <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+          </svg>
+        </span>
+      </Link>
     </>
   );
-}
+};
+
+export default SearchResultsWrapper;
