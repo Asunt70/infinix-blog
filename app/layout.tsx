37efaa1052a7afeaa4069ed100d94a421d6374f1
Asunt70 <sebaj348@gmail.com>
2024-09-25 14:08:26 -0400

    adSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Head from "next/head";
 const inter = Inter({
   subsets: ["latin"],
 });
@@ -18,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7484381705146440"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
